f4f1ef56ee44cd9e759ef76bc570544e98788b21
Olena-prydalna <epridalnaya@gmail.com>
2025-06-02 16:52:52 +0200

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,22 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+
+{/* <header className="flex items-center justify-between px-4 py-2 bg-amber-700 text-slate-100">
+          <p>
+            <Link href="/">Header</Link>
+          </p>
+          <ul>
+            {navItems.map((item, index) => {
+              return (
+                <li key={index}>
+                  <Link href={item.href}>{item.title}</Link>
+                </li>
+              );
+            })}
+          </ul>
+        </header> */}
+
         {children}
       </body>
     </html>
