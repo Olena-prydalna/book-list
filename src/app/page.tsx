dd06a59716cc4ca3d251312ec3c6d717749197a1
Olena-prydalna <epridalnaya@gmail.com>
2025-07-20 20:45:07 +0200

    update style and readme file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="bg-blue-950 flex items-center justify-center flex-col p-15 relative ">
+    <div className="bg-blue-950 flex items-center justify-center flex-col p-15 relative min-h-screen ">
       <Link href="/books" className="flex items-center justify-center">
         <h1 className="text-blue-950 text-5xl font-bold backdrop-blur-sm bg-blue-300/40 rounded-full px-10 py-4 absolute top-5">
           Booklist
